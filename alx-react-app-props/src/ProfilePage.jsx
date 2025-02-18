44de0d3ba18f4ea22350cf7be0786c3128d9ca74
mentesnot <mentesnotassefa0gmail.com>
2025-02-18 06:18:34 +0000

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserDetails from './UserDetails';
+
+function ProfilePage() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <UserDetails />
+    </div>
+  );
+}
+
+export default ProfilePage;
