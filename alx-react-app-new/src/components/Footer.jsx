a1c6b460b69649c214378f86c2ce8172c304373e
Mentesnot Assefa <mentesnotassefa0@gmail.com.com>
2025-02-23 17:24:35 +0000

    Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-<footer>
+<footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
   <p>© 2023 City Lovers</p>
 </footer>
 
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MainContent() {
   return (
     <div>
-        <main>
+        <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
 
