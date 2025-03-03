a389b14ea1ffabb3b1806fe5224b425b307e07c4
Mentesnot Assefa <mentesnotassefa0@gmail.com.com>
2025-03-03 19:50:10 +0000

    ADP

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,15 +1,21 @@
-
-import { Outlet, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 function Profile() {
   return (
     <div>
       <h1>Profile Page</h1>
       <nav>
-        <Link to="details">Profile Details</Link> |{" "}
-        <Link to="settings">Profile Settings</Link>
+        <a href="/profile/details">Profile Details</a> |{" "}
+        <a href="/profile/settings">Profile Settings</a>
       </nav>
-      <Outlet />
+
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 }
