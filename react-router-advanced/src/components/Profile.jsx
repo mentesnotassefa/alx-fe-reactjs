27f0fbd46b26e88995dc495320e897b47731210c
Mentesnot Assefa <mentesnotassefa0@gmail.com.com>
2025-03-03 19:47:45 +0000

    Advanced Routing in React with React Router profile

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet, Link } from "react-router-dom";
+
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <nav>
+        <Link to="details">Profile Details</Link> |{" "}
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Profile;
