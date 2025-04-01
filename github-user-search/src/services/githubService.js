3bec99e5a43c8457ad4543ff2bab99fbccb5be0b
Mentesnot Assefa <mentesnotassefa0@gmail.com>
2025-04-01 10:56:14 +0100

    fetchUserData

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,6 +5,7 @@ const api = axios.create({
   headers: {
     'Accept': 'application/vnd.github.v3+json',
    // 'Authorization': `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+   //fetchUserData
   },
 });
 
